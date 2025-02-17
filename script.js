f6e48fc6af9fc081672be8c7f12be9937b66b598
陳奕名 <joy936219dog@gmail.com>
2025-02-18 00:18:16 +0800

    修正搜尋結果頁點編輯才可以改 然後儲存改為唯讀

--- script.js
+++ script.js
@@ -1,11 +1,22 @@
 function enableEdit() {
-    document.getElementById('parkingSpot').removeAttribute('readonly');
+    //全部的Input 解除readonly
+    //document.getElementById('parkingSpot').removeAttribute('readonly');
+    var x = document.getElementsByTagName("input");
+    for ( var counter = 0; counter < x.length; counter++)
+    {
+       x.item(counter).readOnly = "";
+    }  
     document.getElementById('editBtn').classList.add('d-none');
     document.getElementById('saveBtn').classList.remove('d-none');
 }
 
 function saveEdit() {
-    document.getElementById('parkingSpot').setAttribute('readonly', true);
+    //document.getElementById('parkingSpot').setAttribute('readonly', true);
+    var x = document.getElementsByTagName("input");
+    for ( var counter = 0; counter < x.length; counter++)
+    {
+       x.item(counter).readOnly = "readonly";
+    }
     document.getElementById('saveBtn').classList.add('d-none');
     document.getElementById('editBtn').classList.remove('d-none');
 }
