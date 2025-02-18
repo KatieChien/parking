5408ffa2044436cf7f915a1dbf4b3bfc08773e74
KatieChien <kt.chien@outlook.com>
2025-02-18 22:29:57 +0800

    新增addnew頁面

--- script.js
+++ script.js
@@ -20,3 +20,18 @@ function saveEdit() {
     document.getElementById('saveBtn').classList.add('d-none');
     document.getElementById('editBtn').classList.remove('d-none');
 }
+// 設定租約開始日為當天日期
+document.addEventListener("DOMContentLoaded", function () {
+    document.getElementById("startdate").value = new Date().toISOString().split("T")[0];
+});
+//新增按鈕資料同步google sheet
+document.getElementById("addRecord").addEventListener("click", function () {
+    alert("點擊後會新增資料到 Google Sheets（之後串接 API）");
+});
+
+//index查詢按鈕
+document.getElementById("searchBtn").addEventListener("click", function () {
+    alert("查詢 Google Sheets（之後串接 API）");
+});
+
+
