e60bb75586b164f129e42bc10ee6c3c3627a1a90
KatieChien <kt.chien@outlook.com>
2025-03-03 22:10:24 +0800

    addnew欄位新增
    新增addnew選擇停車場欄位

--- addnew.js
+++ addnew.js
@@ -1,3 +1,27 @@
+//google sheet api串接
+const apiKey = "AIzaSyA13JzQbEfos7UR8PuYSrxpnBBiqdkuE8I"; 
+const spreadsheetId = "1iIdv3ETO9ZFbRyGfIymX21To02LyV30jy9bD-MFN2cA"; 
+const url = `https://sheets.googleapis.com/v4/spreadsheets/${spreadsheetId}?key=${apiKey}`;
+
+fetch(url)
+  .then(response => response.json())
+  .then(data => {
+    const sheets = data.sheets;
+    const dropdown = document.getElementById("sheetDropdown");
+
+    // 先清除所有選項
+    dropdown.innerHTML = "<option value=''>選擇停車場</option>";  // 可以保留一個預設選項
+
+    // 動態添加新的選項
+    sheets.forEach(sheet => {
+      const option = document.createElement("option");
+      option.value = sheet.properties.title;
+      option.textContent = sheet.properties.title;
+      dropdown.appendChild(option);
+    });
+  })
+  .catch(error => console.error("錯誤: ", error));
+
 // 設定租約開始日為當天日期
 document.addEventListener("DOMContentLoaded", function () {
     document.getElementById("startdate").value = new Date().toISOString().split("T")[0];
